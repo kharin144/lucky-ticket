482478eb14c3de4d79487e5a7365378cc5061f3e
K. Harin <happyk@yandex.ru>
2018-02-28 15:50:18 +0500

    it works

--- luckyomp.c
+++ luckyomp.c
@@ -7,41 +7,34 @@
 #include <omp.h>
 #include <time.h>
 
-int main(int argc, char *argv[]){
+int check_args(int argc, char *argv[]);
 
-	if( argc<2 ) exit(1);
+int main(int argc, char *argv[]){
 	
+	check_args(argc, argv);
+
 	//debug
 	//printf("%s\n", argv[1]);
-	//printf("Treads: %d\n", omp_get_num_threads() );
-
+	
 	char *str;
 	str = (char*) malloc( sizeof(char)*strlen(argv[1]) );
 	strcpy(str,argv[1]);
-
-	for(int i=0;i< strlen(argv[1]); i++){
-		if( (!isdigit(str[i])) && (str[i]!='d') )
-			exit(2);
-	}
-
-	int *mass;
-	//int *digits;
-	//int tmp, j;
-	//int sum = 0;
+	
 	int threads;
-	const int half = strlen(str)/2;//amount of digits
+	const int half = strlen(str)/2; //amount of digits
 	const int limit= 9 * half; //maximum sum in part
 	
 	free(str);
-	
+		
+	int *mass;
 	mass   = (int*) malloc( sizeof(int) * (limit+1) );
-	for( int i=0; i<=(limit+1); i++ )
+	
+	for( int i=0; i<(limit+1); i++ ){
 			mass[i]=0;
-
-
-
+	}
+	
 //	#pragma omp parallel firstprivate(sum, digits) private(j,tmp) shared(mass,threads)
-	#pragma omp parallel shared(mass, threads)
+	#pragma omp parallel shared(mass, threads) firstprivate(half, limit)
 	{
 		const int tnum=omp_get_thread_num();
 		//threads= omp_get_num_threads();
@@ -56,7 +49,7 @@ int main(int argc, char *argv[]){
 		
 		#pragma omp barrier
 		// initialisation
-		for( int i=0; i<=(limit+1); i++ )
+		for( int i=0; i<(limit+1); i++ )
 			lmass[i]=0;
 		
 		int sum=0, tmp;
@@ -82,18 +75,44 @@ int main(int argc, char *argv[]){
 		free(lmass);
 
 	}//end of parallel section
-
-	//debug
-	//for(int i=0; i<=(limit+1)*threads; i++) printf("%d ", mass[i]); printf("\n");
 	
+/*	
+	//debug
 	for(int i=0; i<limit+1; i++)
 		printf("%d ", mass[i]);
-	
 	printf("\n");
-	
+*/	
 	//time
 	//printf("%f\n", clock()/(CLOCKS_PER_SEC + 0.) );
-
+	
+	int amount=0;
+	for(int i=0; i<(limit+1); i++){
+		amount+=pow(mass[i], 2);
+	}
+	int power = pow(10, half*2);
+	printf("Amount of Lucky Tickets: %d in %d\n", amount, power);
+	
 	free(mass);
 
+}
+
+
+int check_args(int argc, char *argv[]){
+	if( argc<2 )
+		exit(1);
+		
+	char *str;
+	str = (char*) malloc( sizeof(char)*strlen(argv[1]) );
+	strcpy(str,argv[1]);
+	
+	if(strlen(str)<2)
+		exit(3);
+	
+	for(int i=0;i< strlen(argv[1]); i++){
+		if( (!isdigit(str[i])) && (str[i]!='d') )
+			exit(2);
+	}
+	
+	free(str);
+	return 0;
 }
---
+++ luckyomp.sh
@@ -0,0 +1,2 @@
+gcc luckyomp.c -std=c99 -lm -fopenmp -O1 -o mlucky
+#gcc luckyomp.c -std=c99 -lm -fopenmp -g3 -o mlucky
